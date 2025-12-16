681fb2a597c159c72bf35886c4dae20d5176f4ba
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 22:33:46 +0400

    Update operators.py

--- section 2 /operators.py
+++ section 2 /operators.py
@@ -1,1 +1,14 @@
+arithmetic operation 
++ ,- , * , % , /
+// ( floor division)
+exponent = a**b
 
+
+21/5= 4.2
+but 21//5 = 4
+
+comparison operators
+== , != , > , < 
+
+logical operators
+and , not , or
