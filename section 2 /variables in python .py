08c12bb4a914b56d1dcc32b72ab81e385b8165b4
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 21:20:53 +0400

    Update variables in python .py

--- section 2 /variables in python .py
+++ section 2 /variables in python .py
@@ -12,4 +12,39 @@ print("name", name)
 variables should start with letters or _ and contain letters . no other special characters
 can contains numbers in between
 
+invalid variable names
+3name
+first-name
+haaer@
+
+python is case sensitive 
+python is dynamically typed
+
+
+age=40 //age converted into string and we can do vise versa 
+print(type(age))
+age_str=str(age)
+print(age_str)
+print(type(age_str))
+
+o/p -
+<class 'int'>
+40
+<class 'str'>
+
+age='40'
+int(age)
+this will be converted but not 
+name="aarna"
+int(name) //this cant be converted into int .
+
+
+asking for input 
+age=input("what is your age")
+print(age, type(age))
+
+we can also convert it into int 
+age=int(input("what is your age"))
+print(age, type(age))
+
 
