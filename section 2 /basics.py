3c4e5d8bae5e59d424b36e9998d82d3a739f7cf5
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 19:06:29 +0400

    Update basics.py

--- section 2 /basics.py
+++ section 2 /basics.py
@@ -1,1 +1,32 @@
 
+name="aarna"
+Name="chauhan"
+print(name, Name)
+o/p : aarna chauhan
+
+indentation 
+
+## single line comment 
+
+# line continuation 
+total = 1+2+3 \
++1
+print(total)
+o/p - 7
+
+
+# mulriple things in one line 
+x=40;y=60; z= x+y
+print(z)
+o/p = 100
+
+no need for data type assignment 
+it will understand on it own
+to know the variable we will write type(age)
+
+name error
+a=b
+what is b?
+
+
+
