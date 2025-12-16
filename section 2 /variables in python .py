facbc23ab8ba0c80ebf015e355178cb4f3950cbe
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 19:12:55 +0400

    Create variables in python .py

---
+++ section 2 /variables in python .py
@@ -0,0 +1,15 @@
+
+
+age=20
+height=160.1
+name="aarna"
+
+# printing 
+print("age", age)
+print("height" , height )
+print("name", name)
+
+variables should start with letters or _ and contain letters . no other special characters
+can contains numbers in between
+
+
