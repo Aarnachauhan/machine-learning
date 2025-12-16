3e71b1caab8efbce0112637018b540db0c5277c0
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 23:48:55 +0400

    Update conditional statement.py

--- section 3 /conditional statement.py
+++ section 3 /conditional statement.py
@@ -1,1 +1,14 @@
 
+if , elif , else statement 
+
+nested statements
+num=int(input("enter number"))
+if num>=0:
+    if num%2==0:
+        print("this is an even number")
+    else:
+        print("this is an odd number")
+
+else:
+    print("enter valid number ")
+
