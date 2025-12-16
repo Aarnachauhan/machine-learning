c0a8196cb274eeab2af444cfd5723aade3c108e8
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-16 21:50:28 +0400

    Update and rename basic data types.py to  data types.py

---
+++ section 2 / data types.py
@@ -0,0 +1,14 @@
+
+float , int , str , boolean
+a=10
+b=10
+type(a==b)
+o/p = bool
+
+common errors
+res="hello" + 5
+cannot concatinate , only strings allowed
+
+we can do :
+res="hello" + str(5)
+res="hello5"
