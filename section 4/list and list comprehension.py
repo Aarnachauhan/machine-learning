cc22f797b41e257c8404300e8c61a7c14db562c3
Aarna chauhan <chauhanaarna6@gmail.com>
2025-12-18 00:23:58 +0400

    Create  list and list comprehension.py

---
+++ section 4/list and list comprehension.py
@@ -0,0 +1,39 @@
+list=[]
+print(type(list))
+this will give class list
+
+list=[]
+print(type(list))
+lst=["aarna","jack","john"]
+print(lst)
+lst=[3.02,"aarna",9]
+print(lst)
+
+fruits=["apple","gauva","lichi","mango"]
+print(fruits[0])
+print(fruits[3])
+ans- apple , mango
+
+
+print(fruits[1:])
+gauva to mango will be printed.
+
+append - at the last
+insert(1,"banana") - 1st index pe banana rakh do
+remove - remove the first occurance
+pop - remove and return the last element
+count(banana) - count the no of bananas
+fruits.reverse
+fruits.clear - removes everything from fruits.
+
+
+numbers=[1,2,3,4]
+for num in numbers:
+  print(num)
+
+1
+2
+3
+4
+
+
