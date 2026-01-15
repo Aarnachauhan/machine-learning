6109b9bffed8e9ba5cb004da25c60515ac06a2f5
Aarna chauhan <chauhanaarna6@gmail.com>
2026-01-15 20:01:02 +0530

    Update lec 39.py

--- section 11/lec 39.py
+++ section 11/lec 39.py
@@ -21,4 +21,29 @@ dog1=Dog("buddy",4)
 dog1.bark()
 
 o/p -> buddy says woof
+--------------------------------------------------------------------------------------------------------------
+class Account:
+    def __init__ (self , owner , balance=0):
+        self.owner=owner
+        self.balance=balance
+
+    def deposit(self,amount):
+        self.balance+=amount
+        print(f"{amount} is the amount deposited")
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("insufficient balance")
+        else:
+            balance-=balance
+
+    
+    
+acc=Account("aarna",5000)
+print(acc.balance)
+
+acc.deposit(100)
+
+print(acc.balance)
+
 
