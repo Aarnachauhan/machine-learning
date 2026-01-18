e925e3e8fcd0efaf86789441961544a6f797b048
Aarna chauhan <chauhanaarna6@gmail.com>
2026-01-18 19:30:59 +0530

    Update logging pract.py

--- section15 logging in py/logging pract.py
+++ section15 logging in py/logging pract.py
@@ -1,1 +1,26 @@
+python logging ->
 
+-------------------------------------------
+
+import logging
+#config the basic logging setting
+logging.basicConfig(level=logging.DEBUG)
+
+#log messages with diff severity level message
+logging.debug("this is logging message")
+logging.info("this is info message")
+logging.warning("this is warning message")
+logging.error("this is error message")
+logging.critical("this is critical message")
+  
+o/p
+DEBUG:root:this is logging message
+INFO:root:this is info message
+WARNING:root:this is warning message
+ERROR:root:this is error message
+CRITICAL:root:this is critical message
+
+---------------------------------------------------------
+
+  
+  
